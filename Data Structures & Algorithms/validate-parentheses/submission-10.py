2f52ef514e869017e287d9ed9d9e9f1cf16930db
pranav padmapada <94914458+pranav1233@users.noreply.github.com>
2026-07-05 15:38:20 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        valid = []
+        combo = {")":"(","}":"{","]":"["}
+        if len(s)%2 != 0:
+            return False
+
+        for p in s:
+            if p not in combo:
+                valid.append(p)
+            else:
+                if not len(valid) > 0:
+                    return False
+                pop = valid.pop()
+
+                if pop != combo[p]:
+                    return False
+
+        return True if len(valid) == 0 else False
